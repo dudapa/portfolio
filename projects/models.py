2086b973b068ccd185062ad615bf8fdf5de92b20
Patrik Duda <patrikduda94@gmail.com>
2022-08-11 16:48:57 +0200

    Buid Project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    technologies = models.TextField()
+    view_code = models.TextField()
+    is_live_code = models.BooleanField(default=True)
+    live_code = models.TextField(blank=True)
+    project_image = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
