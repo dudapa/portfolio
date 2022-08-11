5828c574bf110074e6292b85f9c072768ef1c9d1
Patrik Duda <patrikduda94@gmail.com>
2022-08-11 15:39:18 +0200

    Set up postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
