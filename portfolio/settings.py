7345fa10394254f3c85c11852dedb5c00dbd04f9
Patrik Duda <patrikduda94@gmail.com>
2023-10-09 12:51:47 +0200

    Modify ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = list(config('ALLOWED_HOST', default=['localhost','127.0.0.1']))
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['localhost','127.0.0.1'], cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
